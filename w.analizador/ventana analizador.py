17bf110886d59ee878e9785a2b8c0bde6324e14a
Esteban-A3 <sanchezesteban0302@gmail.com>
2026-03-20 15:20:59 -0600

    Se cambio el nombre

--- w.analizador/ventana analizador.py
+++ w.analizador/ventana analizador.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 
-ventana_f = tk.Tk()
-ventana_f.title("Analizador de Números")
-ventana_f.geometry("600x400")
-ventana_f.config(bg="tomato")
-ventana_f.attributes(alpha=0.95)
-ventana_f.resizable(False,False)
+ventana_a = tk.Tk()
+ventana_a.title("Analizador de Números")
+ventana_a.geometry("600x400")
+ventana_a.config(bg="tomato")
+ventana_a.attributes(alpha=0.95)
+ventana_a.resizable(False,False)
 
-factorizar= tk.Frame(ventana_f)
+factorizar= tk.Frame(ventana_a)
 factorizar.config(bg="indianred1", bd=10)
 
 m_bienvenida= tk.Label(factorizar, text="Introdusca un número entero para analizar")
@@ -30,4 +30,4 @@ entrada_f.pack()
 resultado.pack()
 analizar.pack()
 
-ventana_f.mainloop()
+ventana_a.mainloop()
