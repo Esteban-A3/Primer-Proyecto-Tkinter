6da5b1537838fe9f49d63b9a84619bb916ecf66c
Esteban-A3 <sanchezesteban0302@gmail.com>
2026-03-20 15:19:10 -0600

    Se finalizo la ventana

--- w.factores/ventana factores.py
+++ w.factores/ventana factores.py
@@ -1,25 +1,33 @@
 import tkinter as tk
 
 ventana_f = tk.Tk()
-ventana_f.title("Factores")
-ventana_f.geometry("500x300")
-ventana_f.minsize(400,250)
-ventana_f.maxsize(600,400)
+ventana_f.title("Analizador de Números")
+ventana_f.geometry("600x400")
 ventana_f.config(bg="tomato")
 ventana_f.attributes(alpha=0.95)
+ventana_f.resizable(False,False)
 
 factorizar= tk.Frame(ventana_f)
-factorizar.config(width=300 ,height=200 , bg="blue", bd=5)
-factorizar.pack()
+factorizar.config(bg="indianred1", bd=10)
 
-m_bienvenida= tk.Label(factorizar, text="Introdusca un numero entero para analizar")
-m_bienvenida.pack()
+m_bienvenida= tk.Label(factorizar, text="Introdusca un número entero para analizar")
+m_bienvenida.config(bg="indianred1",fg="gold", font=("Impact",15))
 
 entrada_f= tk.Entry(factorizar)
-entrada_f.pack()
+entrada_f.config(bg="azure2", fg="gray25", font=("Impact",12))
+entrada_f.insert(0, "Ejemplo: 10")
+
+analizar=tk.Button(factorizar, text="Analizar")
+analizar.config(bg="azure2", fg="gray23", font=("Impact", 12))
 
 resultado=tk.Label(factorizar, text="")
-resultado.pack()
+resultado.config(bg="indianred1", fg="gold", font=("impact", 13, "italic"))
 
+
+factorizar.place(relx=0.19,rely=0.30)
+m_bienvenida.pack()
+entrada_f.pack()
+resultado.pack()
+analizar.pack()
 
 ventana_f.mainloop()
