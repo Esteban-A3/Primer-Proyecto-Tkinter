c999ee46df730d53f0888b3a207f25c50290aa77
Esteban-A3 <sanchezesteban0302@gmail.com>
2026-03-20 14:10:55 -0600

    Midificacion de botones y texto

--- w.factores/ventana factores.py
+++ w.factores/ventana factores.py
@@ -6,10 +6,20 @@ ventana_f.geometry("500x300")
 ventana_f.minsize(400,250)
 ventana_f.maxsize(600,400)
 ventana_f.config(bg="tomato")
-ventana_f.attributes(alpha=0.9)
+ventana_f.attributes(alpha=0.95)
 
 factorizar= tk.Frame(ventana_f)
 factorizar.config(width=300 ,height=200 , bg="blue", bd=5)
 factorizar.pack()
+
+m_bienvenida= tk.Label(factorizar, text="Introdusca un numero entero para analizar")
+m_bienvenida.pack()
+
+entrada_f= tk.Entry(factorizar)
+entrada_f.pack()
+
+resultado=tk.Label(factorizar, text="")
+resultado.pack()
+
 
 ventana_f.mainloop()
