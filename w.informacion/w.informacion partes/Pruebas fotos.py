a35ca7c94aafcdb70baa3aa82e71b36e3582bd8e
Esteban-A3 <sanchezesteban0302@gmail.com>
2026-03-21 23:42:31 -0600

    pruebas con imagenes

---
+++ w.informacion/w.informacion partes/Pruebas fotos.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+ 
+ventanai = tk.Tk()
+imagen = tk.PhotoImage(file="w.informacion partes/Programador.png")
+imagenl = tk.Label(ventanai, image=imagen)
+imagenl.pack()
+
+
+ventanai.mainloop()
