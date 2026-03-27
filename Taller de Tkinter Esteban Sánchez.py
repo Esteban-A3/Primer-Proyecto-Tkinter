4bdc748fc07da4240c910fdf402eb2d692908563
Esteban-A3 <sanchezesteban0302@gmail.com>
2026-03-26 21:41:15 -0600

    se hicieron las ventanas de informacion y se arreglo la ventana de analizador de numeros

--- Taller de Tkinter Esteban Sánchez.py
+++ Taller de Tkinter Esteban Sánchez.py
@@ -2,23 +2,215 @@ import tkinter as tk
 import pygame as pg
 from tkinter import Toplevel
 
+def analisis_pares(num):
+    if not isinstance(num,int):
+        return "Error: Por favor introducir un número entero"
+    if num<=0:
+        return  "No posee factores positivos"
+    else:
+        return analisis_pares_aux(num,1)
+
+def analisis_pares_aux(num, i):
+    if i > int(num**0.5):
+        return ()
+
+    elif num%i == 0:
+        return (((i, num//i),) + analisis_pares_aux(num, i+1))
+
+    else:
+        return analisis_pares_aux(num,i+1)
+
+pg.mixer.init()
+paused = False
+music = False
+
+def playmusic():
+    global paused
+    global music
+    if music== False:
+        if paused==True:
+            pg.mixer.music.unpause()
+            paused=False
+            music=True
+        else:
+            pg.mixer.music.load("Proyecto Multimedia/Me and Michael.mp3")
+            pg.mixer.music.play()
+    else:
+        ()
+
+def pausemusic():
+    global paused
+    global music
+    pg.mixer.music.pause()
+    paused=True
+    music=False
+
+def stopmusic():
+    global paused
+    global music
+    pg.mixer.music.stop()
+    paused=False
+    music=False
+
+def restardmusic():
+    global paused
+    global music
+    pg.mixer.music.stop()
+    pg.mixer.music.load("w.informacion partes/Me and Michael.mp3")
+    pg.mixer.music.play()
+    paused = False
+    music = True
+
 
 ventana_actual = None
 
-def abrir_ventana(nombre):
+def abrir_ventana_a():
     global ventana_actual
     
     if ventana_actual is not None and ventana_actual.winfo_exists():
         ventana_actual.destroy()
     
-    nueva_ventana = Toplevel(ventana_m)
-    nueva_ventana.title(f"Ventana: {nombre}")
-    nueva_ventana.geometry("400x300")
-    nueva_ventana.config(bg="white")
+    ventana_a = Toplevel(ventana_m)
+    ventana_a.title("Analizador de Números")
+    ventana_a.geometry("600x400")
+    ventana_a.config(bg="tomato")
+    ventana_a.attributes(alpha=0.95)
+    ventana_a.resizable(False,False)
+
+    sideanalizar= tk.Frame(ventana_a)
+    sideanalizar.config(bg="indianred1", bd=10, width=450, height=200)
+
+    m_bienvenida= tk.Label(sideanalizar, text="Introdusca un número entero para analizar")
+    m_bienvenida.config(bg="indianred1",fg="gold", font=("Impact",15))
+
+    entrada_a= tk.Entry(sideanalizar)
+    entrada_a.config(bg="azure2", fg="gray25", font=("Impact",12))
+    entrada_a.insert(0, "Ejemplo: 10")
+    def pares():
+        valor = entrada_a.get()
+        if not valor or valor == "Ejemplo: 10":
+            resultado.config(text="⚠️ Por favor ingrese un número")
+            return
+        elif valor.isdigit():
+            num = int(valor)
+            if num > 100000:
+                resultado.config(text="⚠️ Límite excedido: Intenta con un numero menor (Máx: 100,000)") #Limite para proteger interfaz. El verdadero limite de la funcion es 1 millon
+                return
+            else:
+                res = analisis_pares(num)
+                resultado.config(text=f"Factores de {num} = {res}")
+        else:
+            resultado.config(text="❌ Entrada inválida, debe ser un entero positivo")
+    analizar=tk.Button(sideanalizar, text="Analizar", command=pares)
+    analizar.config(bg="azure2", fg="gray23", font=("Impact", 12))
+
+    resultado=tk.Label(sideanalizar, text="")
+    resultado.config(bg="indianred1", fg="gold", font=("impact", 13, "italic"), wraplength=350, justify="center")
+    def cerraranalizador():
+        ventana_a.destroy()
+
+    BotonCA = tk.Button(ventana_a, text="Cerrar", command=cerraranalizador, bg="white", fg="grey60", font=("Impact", 12), relief=tk.RAISED)
+
+    sideanalizar.place(relx=0.5,rely=0.5, anchor="center")
+    sideanalizar.pack_propagate(False)
+    m_bienvenida.pack()
+    entrada_a.pack()
+    resultado.pack()
+    analizar.pack()
+    BotonCA.place(relx=0.50,rely=0.90, anchor="center")
+
+    ventana_actual = ventana_a
+
+def abrir_ventana_i():
+    global ventana_actual
     
-    tk.Label(nueva_ventana, text=f"Ventana {nombre}", bg="white", font=("Impact", 20)).pack(pady=50)
+    if ventana_actual is not None and ventana_actual.winfo_exists():
+        ventana_actual.destroy()
     
-    ventana_actual = nueva_ventana
+    ventana_i = Toplevel(ventana_m)
+    ventana_i.title("Ventana de información")
+    ventana_i.geometry("700x500")
+    ventana_i.config(bg="sky blue")
+    ventana_i.resizable(False, False)
+    ventana_i.attributes(alpha=0.95)
+
+    block_nce = tk.Canvas(ventana_i, bg="light cyan1", bd=5, width=400, height=100)
+    block_bio = tk.Canvas(ventana_i, bg="light cyan2", bd=5, width=400, height=200)
+    block_music = tk.Canvas(ventana_i, bg="light cyan3", bd=5, width=400, height=100)
+    block_foto = tk.Frame(ventana_i, bg="pale green1", bd=8, width=150, height=200)
+    block_l = tk.Canvas(ventana_i, bg="pale green2", bd=8, width=250, height=130)
+    musiccentertext = tk.Frame(block_music,bg="light cyan3")
+
+    def cerrarVentanaInformacion():
+        ventana_i.destroy()
+
+    botonCi = tk.Button(ventana_i,text="X",command= cerrarVentanaInformacion)
+
+    nombre = tk.Label(block_nce, text="Esteban Sánchez Ledezma", bg="light cyan1", fg="grey60", font=("Impact", 15))
+    carnet = tk.Label(block_nce, text="2026108570", bg="light cyan1", fg="grey60", font=("Impact", 15))
+    edad = tk.Label(block_nce, text="19 años", bg="light cyan1", fg="grey60", font=("Impact", 15))
+
+    programadorM = tk.PhotoImage(file="Proyecto Multimedia/Programador.png")
+    sideP= tk.Label(block_foto, image=programadorM)
+    lugar= tk.PhotoImage(file="Proyecto Multimedia/lugar.png")
+    sideL= tk.Label(block_l, image=lugar)
+    textoL = tk.Label(block_l, text="Lugar donde vive:", bg="pale green2", fg="grey60", font=("Impact", 15) )
+
+    textobiografia = """Soy técnico electromecánico graduado y actualmente estudiante de Ingeniería en Computadores en el Tecnológico de Costa Rica. 
+    Además, he complementado mi formación en el área de redes, alcanzando el nivel de CCNA 3 con certificación académica. 
+
+    Me caracterizo por ser una persona proactiva, con gran interés en la tecnología, la programación y las redes. 
+    Busco constantemente fortalecer mis conocimientos y adquirir nuevas habilidades que me permitan crecer profesionalmente.
+    Mi objetivo es desarrollarme como ingeniero en computación y aportar soluciones innovadoras en el ámbito tecnológico."""
+
+    biografia = tk.Label(block_bio, text=textobiografia,font=("Time new Roman", 9), wraplength=380,fg="grey40", justify="left", bg="light cyan2")
+    biografia.pack(padx=5, pady=5)
+
+    bandaygenero = """Nombre de la banda: MGMT 
+    Genero: Indi Rock
+    Canción: Me and Michael"""
+
+    textoM = tk.Label(block_music, text=bandaygenero, bg="light cyan3", fg="grey60", font=("Impact", 12))
+    Ibanda = tk.PhotoImage(file="Proyecto Multimedia/banda.png")
+    sideIb = tk.Label(musiccentertext, image=Ibanda)
+    botonBack = tk.Button(musiccentertext, text="⏮",command=restardmusic)
+    botonPlay = tk.Button(musiccentertext, text="▶", command=playmusic)
+    botonPause = tk.Button(musiccentertext, text="⏸", command=pausemusic)
+    botonstop = tk.Button(musiccentertext, text="⏹", command=stopmusic)
+
+    block_nce.place(relx=0.3, rely=0.16, anchor="center")
+    block_nce.propagate(False)
+
+    block_bio.place(relx=0.3, rely=0.5, anchor="center")
+    block_bio.propagate(False)
+
+    block_music.place(relx=0.3, rely=0.84, anchor="center")
+    block_music.propagate(False)
+
+    block_foto.place(relx=0.80, rely=0.25, anchor="center")
+    block_foto.propagate(False)
+
+    block_l.place(relx=0.80, rely=0.70, anchor="center")
+    block_l.propagate(False)
+
+    botonCi.place(x=670, y=8)
+    nombre.pack()
+    carnet.pack()
+    edad.pack()
+    sideP.pack()
+    textoL.pack()
+    sideL.pack()
+    textoM.pack()
+
+    musiccentertext.pack(expand=True)
+    sideIb.pack(side="left", anchor="center",padx=10)
+    botonBack.pack(side="left", padx=5)
+    botonPlay.pack(side="left", padx=5)
+    botonPause.pack(side="left",padx=5)
+    botonstop.pack(side="left", padx=5)
+    ventana_actual = ventana_i
+
+
 
 
 
@@ -41,9 +233,9 @@ Mensaje.pack()
 BlockVentanas = tk.Frame(ventana_m, bg="aquamarine")
 BlockVentanas.pack(pady=20)
 
-botonwA = tk.Button(BlockVentanas, width=25, height=10, text="Analizador de Números", bg="tomato", fg="White", font=("Impact"), command=lambda: abrir_ventana("Analizador"))
-botonwI = tk.Button(BlockVentanas, width=25, height=10, text="Información del programador", bg="sky blue", fg="White", font=("Impact"), command=lambda: abrir_ventana("Información"))
-BotonwP = tk.Button(BlockVentanas, width=25, height=10, text="Animación", bg="olivedrab1", fg="White", font=("Impact"), command=lambda: abrir_ventana("Animación"))
+botonwA = tk.Button(BlockVentanas, width=25, height=10, text="Analizador de Números", bg="tomato", fg="White", font=("Impact"), command=lambda: abrir_ventana_a())
+botonwI = tk.Button(BlockVentanas, width=25, height=10, text="Información del programador", bg="sky blue", fg="White", font=("Impact"), command=lambda: abrir_ventana_i())
+BotonwP = tk.Button(BlockVentanas, width=25, height=10, text="Animación", bg="olivedrab1", fg="White", font=("Impact"), command=lambda: abrir_ventana())
 
 
 botonwA.grid(row=0, column=0, padx=10)
